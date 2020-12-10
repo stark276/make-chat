438b65eb7ee3ae7b77f7f38c4b591d29539f3c4c
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-12-09 19:07:08 -0800

    Messages go to correct channel

--- public/index.js
+++ public/index.js
@@ -18,19 +18,20 @@ $(document).ready(()=>{
   });
 
   $('#send-chat-btn').click((e) => {
-    e.preventDefault();
-    // Get the message text value
-    let message = $('#chat-input').val();
-    // Make sure it's not empty
-    if(message.length > 0){
-      // Emit the message with the current user to the server
-      socket.emit('new message', {
-        sender : currentUser,
-        message : message,
-      });
-      $('#chat-input').val("");
-    }
-  });
+  e.preventDefault();
+  // Get the client's channel
+  let channel = $('.channel-current').text();
+  let message = $('#chat-input').val();
+  if(message.length > 0){
+    socket.emit('new message', {
+      sender : currentUser,
+      message : message,
+      //Send the channel over to the server
+      channel : channel
+    });
+    $('#chat-input').val("");
+  }
+});
 
   //socket listeners
   socket.on('new user', (username) => {
@@ -39,13 +40,19 @@ $(document).ready(()=>{
   })
   //Output the new message
 socket.on('new message', (data) => {
-  $('.message-container').append(`
-    <div class="message">
-      <p class="message-user">${data.sender}: </p>
-      <p class="message-text">${data.message}</p>
-    </div>
-  `);
+  //Only append the message if the user is currently in that channel
+  let currentChannel = $('.channel-current').text();
+  if(currentChannel == data.channel){
+    $('.message-container').append(`
+      <div class="message">
+        <p class="message-user">${data.sender}: </p>
+        <p class="message-text">${data.message}</p>
+      </div>
+    `);
+  }
 })
+
+
 socket.on('get online users', (onlineUsers) => {
   //You may have not have seen this for loop before. It's syntax is for(key in obj)
   //Our usernames are keys in the object of onlineUsers.
