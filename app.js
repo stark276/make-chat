e6d354a7afb629901e67c2621703b078d61590c9
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-12-09 17:05:04 -0800

    Users can join and leave chats

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
+//We'll store our online users here
+let onlineUsers = {};
 io.on("connection", (socket) => {
-  // This file will be read on new socket connections
-  require('./sockets/chat.js')(io, socket);
+  // Make sure to send the users to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers);
 })
 
 const exphbs  = require('express-handlebars');
--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ $(document).ready(()=>{
 
   //Keep track of the current user
   let currentUser;
+  socket.emit('get online users');
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
@@ -45,5 +46,20 @@ socket.on('new message', (data) => {
     </div>
   `);
 })
+socket.on('get online users', (onlineUsers) => {
+  //You may have not have seen this for loop before. It's syntax is for(key in obj)
+  //Our usernames are keys in the object of onlineUsers.
+  for(username in onlineUsers){
+    $('.users-online').append(`<div class="user-online">${username}</div>`);
+  }
+})
+//Refresh the online user list
+socket.on('user has left', (onlineUsers) => {
+  $('.users-online').empty();
+  for(username in onlineUsers){
+    $('.users-online').append(`<p>${username}</p>`);
+  }
+});
+
 
 })
